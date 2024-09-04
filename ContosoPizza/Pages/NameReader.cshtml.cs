946d961295fcb14996bc646bd076da0b5303cfaf
Eiccari <p32157@edu.sasky.fi>
2024-09-04 11:00:22 +0300

    Lisäsin uuden sivun

---
+++ ContosoPizza/Pages/NameReader.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ContosoPizza.pages
+{
+    public class NameReaderModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
